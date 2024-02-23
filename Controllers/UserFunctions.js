aeab050b246746b66f0e98ac25a5811704bf7df4
Userded-19 <abinav10102@gmail.com>
2024-02-23 11:15:21 +0530

    Fix required field in UserModal.js and update MongoDB connection in server.js

---
+++ Controllers/UserFunctions.js
@@ -0,0 +1,7 @@
+const userSchema = require("../modals/UserModal");
+
+const LoginUser = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+  } catch (err) {}
+};
--- server.js
+++ server.js
@@ -5,7 +5,22 @@ const morgan = require("morgan");
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect(
-  "mongodb+srv://admin:hello123@cluster0.obs63zv.mongodb.net/",
-  {}
-);
+mongoose
+  .connect("mongodb+srv://admin:hello123@cluster0.obs63zv.mongodb.net/")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "This is backend SDK of youClass " });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
